45898ea185b8ca99e81daec71463705899f91ec4
ahmadeleiwa <ahmadilawa@gmail.com>
2023-12-19 17:16:38 +0200

    Remove useState

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,4 +1,4 @@
-import { FC, MouseEvent, useEffect, useRef, useState } from "react";
+import { FC, MouseEvent, useEffect, useRef } from "react";
 import style from "./style.module.css";
 interface props {}
 
