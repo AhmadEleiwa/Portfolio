da8cee9de8a917a77190028e2b5579bcf80df541
ahmadeleiwa <ahmadilawa@gmail.com>
2024-05-21 18:53:11 +0300

    Remove the unused code

--- src/component/Profile/index.tsx
+++ src/component/Profile/index.tsx
@@ -3,9 +3,9 @@ import PeronsalCard from "./PeronsalCard";
 import style from "./style.module.css";
 import SkillCards from "./SkillCards";
 
-const Profile: FC<props> = ({rf}) => {
+const Profile: FC = () => {
   return (
-    <div ref={ref}>
+    <div>
       <h1 style={{ width: "75%", margin: "0em auto" }}>My Profile</h1>
       <div className={style.container}>
         <PeronsalCard />
